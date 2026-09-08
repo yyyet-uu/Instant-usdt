ae3f1eb8add273dd96589149d5bae8b1ac8bf5c8
yyyet-uu <yafet.tech0990@gmail.com>
2026-09-08 15:57:17 +0300

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,1 @@
+import "./globals.css"; export default function Layout({children}:{children:React.ReactNode}){return <html><head><script src="https://telegram.org/js/telegram-web-app.js"/></head><body>{children}</body></html>}
